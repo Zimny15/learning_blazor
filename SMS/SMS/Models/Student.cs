e466093f0364c2f332bb9d71485843137f4a1795
Jakub Zima <jzima@softguard.pl>
2025-05-19 15:17:58 +0200

    Creation StudentsManagementSystem

---
+++ SMS/SMS/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS.Models;
+
+public class Student
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "First Name is required")]
+    public string FirstName { get; set; } = string.Empty;
+
+
+}
